ade3a71568cb1cff7aa0bf545583bd6edce45329
Vladimir_VS <vladimir-work93@yandex.ru>
2026-02-11 23:56:53 +0300

    Add login.html, register.html, forgot_password.html

--- pharmShelf/users/urls.py
+++ pharmShelf/users/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('forgot_password/', views.forgot_password_view, name='forgot_password'),
 ]
--- pharmShelf/users/views.py
+++ pharmShelf/users/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'users/login.html')
+
+def register_view(request):
+    return render(request, 'users/register.html')
+
+def forgot_password_view(request):
+    return render(request, 'users/forgot_password.html')
