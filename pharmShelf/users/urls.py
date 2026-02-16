61fac516cff86552872aa37190db2a607336d70f
Vladimir_VS <vladimir-work93@yandex.ru>
2026-02-16 06:01:13 +0300

    Added django links on user pages

--- pharmShelf/users/urls.py
+++ pharmShelf/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('forgot_password/', views.forgot_password_view, name='forgot_password'),
